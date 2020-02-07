c1c8336b48f862cae3bcc84723f48bddcd30f129
root <root@node-0.sepy-qv64633.sysarch-pg0.clemson.cloudlab.us>
2020-02-07 13:58:48 -0500

    added two files for testing with HUGEpages

---
+++ HUGE_test_freq_intel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+g++ -std=c++11 -O -mrtm tsxtest.cpp -o freq.out -lpthread
+
+for i in 1 2 3 4 5 6 7 8 9 10 20 30 40 50 100
+do
+LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./freq.out $i >> HUGE_freq_report.txt
+done
+
+exit 0
---
+++ HUGE_test_mem_intel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+g++ -std=c++11 -O -mrtm tsxtest.cpp -o mem.out -lpthread
+
+for i in 1 2 4 8 16 32 64 128 256 512 1024 2048
+do
+LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes ./mem.out $i >> HUGE_mem_report.txt
+done
+
+exit 0
+
